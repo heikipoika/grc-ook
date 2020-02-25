323d0d6cc60b533c06a26f8074e2fa33a8cb4547
heikipoika <pi@pisdr>
2020-02-25 20:52:15 +0000

    added sink

---
+++ epy_block_0.py
@@ -0,0 +1,54 @@
+"""
+Embedded Python Blocks:
+
+Each time this file is saved, GRC will instantiate the first class it finds
+to get ports and parameters of your block. The arguments to __init__  will
+be the parameters. All of them are required to have default values!
+"""
+
+import numpy as np
+import sys
+
+from gnuradio import gr
+
+
+class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
+    """Embedded Python Block example - a simple multiply const"""
+
+    def __init__(self):  # only default arguments here
+        """arguments to this function show up as parameters in GRC"""
+        gr.sync_block.__init__(
+            self,
+            name='Deserialize PrintByte Sink',   # will show up in GRC
+            in_sig=[np.byte, np.byte], 
+            out_sig=None
+        )
+        # if an attribute with the same name as a parameter is found,
+        # a callback is registered (properties work, too).
+        #self.example_param = example_param
+	self.buffer = np.byte(0)
+	self.count = 0
+
+    def work(self, input_items, output_items):
+        """example: multiply with constant"""
+	if np.any(input_items[1][:] > 0):
+        	for i in range (1, len(input_items[0])):
+			if input_items[1][i] & 0x01:
+				#output_items[0][i] = (output_items[0][i-1] << 1) | input_items[0][i]
+				self.buffer = (self.buffer << 1) | (~input_items[0][i] & 0x01)
+				#print "strobe", input_items[1][i]
+				self.count = self.count + 1
+				#print self.count
+				if self.count == 8:
+					#output_items[0][:] = self.buffer
+					sys.stdout.write("%x " % (self.buffer)) 		
+					self.buffer = 0
+					self.count = 0		
+		#print "out", output_items[0][i]
+		#print "out", self.buffer		
+		#print "in", input_items[1][i]	
+	#else:
+	#print "nop"
+	#print input_items[0][:]	
+	#print self.count, output_items[0][:]
+        return len(input_items[0])
---
+++ epy_block_1.py
@@ -0,0 +1,95 @@
+"""
+Serialize source
+
+Takes a predefined string, and outputs it as a serial OOK coded bitsream.....
+"""
+
+import numpy as np
+import sys
+
+from gnuradio import gr
+
+
+class blk(gr.sync_block):
+
+    def __init__(self, bitsInLast = 1, repeat = 5, gap = 40):
+        """
+        bitsInLast - Number of bits to send from tha last byte
+        repeat - number of times to repeat the message
+        gap = the gap to use between messages
+        """
+        
+        gr.sync_block.__init__(
+            self,
+            name='Serialize Source', 
+            in_sig=[np.int16], 
+            out_sig=[np.byte]
+        )
+        
+        self.bitsInLast = bitsInLast
+	self.items = [np.byte(0xff), np.byte(0xff), np.byte(0xea), np.byte(0x80)] #ea on eb off
+        self.sent = 0
+	self.repeat = repeat
+        self.gap = gap
+
+    def work(self, input_items, output_items):
+            
+        retcnt = 0
+        if ~self.sent & input_items[0][0] & 0x01:
+        
+                if len(output_items[0]) > (len(self.items)*8*5+self.gap)*self.repeat:
+                        
+                        for x in range(0, self.repeat):
+                                print x
+                                #output_items[0][retcnt] = 1
+                                #output_items[0][retcnt+1:retcnt+11] = 0
+                                #retcnt = retcnt + 11
+                                for i in range(0, len(self.items)):
+                                        elem = self.items[i]
+                                        last = 8
+                                        if i == len(self.items)-1:
+                                                last = self.bitsInLast
+                                        #print i
+                                        for n in range(0,last):
+                                                #sys.stdout.write("%i " % (n))
+                                                #self.buffer.append(self.items[i] & 0x01) # [i*7+n]
+                                                if elem & 0x80:
+                                                        output_items[0][retcnt] = 1
+                                                        output_items[0][retcnt+1:retcnt+5] = 0
+                                                        retcnt = retcnt + 5
+                                                        #output_items[0][retcnt] = 1
+                                                        #output_items[0][retcnt+1:retcnt+6] = 0
+                                                        #retcnt = retcnt + 6 
+                                                else:
+                                                        output_items[0][retcnt:retcnt+3] = 1
+                                                        output_items[0][retcnt+5] = 0 
+                                                        retcnt = retcnt + 5 
+                                                        #output_items[0][retcnt] = 1
+                                                        #output_items[0][retcnt+1:retcnt+3] = 0 
+                                                        #retcnt = retcnt + 3 
+                                                #output_items[0][i*7+n] = elem & 0x01
+                                                elem =  elem << 1
+                                                #retcnt = retcnt + 1
+                                                #self.count = self.count + 1
+                                #output_items[0][retcnt] = 1
+                                #retcnt = retcnt + 1
+                                output_items[0][retcnt:retcnt+self.gap] = 0
+                                retcnt = retcnt + self.gap         
+                                
+                        self.sent = 1
+                        #print self.sent
+        elif ~input_items[0][0] & 0x01:
+                self.sent = 0
+                output_items[0][:] = 0
+                retcnt = len(output_items[0]) 
+        else:
+                output_items[0][:] = 0
+                retcnt = len(output_items[0]) 
+        #retcnt = self.count
+        #self.count = 0
+        #print self.buffer
+        #output_items[0][0:len(self.buffer)] = 1
+        #output_items[0][:] = 1
+        #self.buffer[:] = []
+        
+        return retcnt
